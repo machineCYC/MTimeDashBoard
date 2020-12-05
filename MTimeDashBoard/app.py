70b1c39a38659b61fefe5e4c2be211a80a6ca745
YenChen <yenchen0416@gmail.com>
2020-12-05 23:45:03 +0800

    fix: show figure size bigger than key number size slowly

--- MTimeDashBoard/app.py
+++ MTimeDashBoard/app.py
@@ -22,18 +22,19 @@ app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
 DATA_DIR = os.path.join(
     os.path.dirname(os.path.dirname(os.path.abspath(__file__))), "Data"
 )
-dataset_names = [f for f in os.listdir(DATA_DIR) if not f.endswith(".gitkeep")]
+DATASET_NAMES = [f for f in os.listdir(DATA_DIR) if not f.endswith(".gitkeep")]
 
 
 def generate_plot_data(id_index):
     id_name = available_ids[id_index]
-    sub_data = df_bar[df_bar["stock_id"] == id_name]
+    sub_data = DATAFRAME[DATAFRAME["stock_id"] == id_name]
     # return {'x':sub_data["date"].index, 'y':sub_data["close"], 'type':'scatter','name':id_name}
     return go.Scatter(x=sub_data["date"], y=sub_data["close"], mode="lines")
     # return px.line(sub_data, x="date", y="close")
 
 
-def get_subplots_fig(show_columns, show_figures):
+def get_subplots_fig(show_figures, show_columns):
+    show_figures = min(show_figures, NBR_KEY)
     quotient = int(show_figures // show_columns)
     remainder = int(show_figures % show_columns)
     show_rows = quotient + 1 if remainder else quotient
@@ -60,9 +61,8 @@ def get_subplots_fig(show_columns, show_figures):
             print(f"id_index:{id_index}, ({row}, {col})")
             fig.append_trace(generate_plot_data(id_index), row + 1, col + 1)
     fig["layout"]["margin"] = {"b": 0, "r": 0, "l": 0, "t": 50}
+    fig["layout"]["showlegend"] = False
 
-    # fig["layout"]["height"] = 200
-    # fig["layout"]["width"] = 250
     # fig["layout"].update(
     #     paper_bgcolor="#18252E", plot_bgcolor="#18252E"
     # )
@@ -70,31 +70,38 @@ def get_subplots_fig(show_columns, show_figures):
 
 
 app.layout = html.Div(
-    [
+    children  = [
         html.Div(
-            [
-                "dataset:",
+            children = [
+                "dataset: ",
                 dcc.Dropdown(
                     id="data_name_input",
-                    options=[{"label": i, "value": i} for i in dataset_names],
-                    value=dataset_names[0],
+                    options=[{"label": i, "value": i} for i in DATASET_NAMES],
+                    value=DATASET_NAMES[0],
+                    style={"width": "50%", "display": "inline-block"}
                 ),
             ],
-            style={"width": "48%"},
+            style={"width": "30%", "display": "inline-block"},
         ),
         html.Div(
-            [
+            children = [
                 "show_figures: ",
-                dcc.Input(id="show_figures", value=min(20, 5), type="text"),
+                dcc.Input(
+                    id="show_figures", value=min(20, 5), type="text",
+                    style={"width": "20%", "display": "inline-block"}
+                ),
             ],
-            style={"width": "24%", "display": "inline-block"},
+            style={"width": "15%", "display": "inline-block"},
         ),
         html.Div(
-            [
+            children = [
                 "show_columns: ",
-                dcc.Input(id="show_columns", value=2, type="text"),
+                dcc.Input(
+                    id="show_columns", value=2, type="text",
+                    style={"width": "20%", "display": "inline-block"}
+                ),
             ],
-            style={"width": "24%", "display": "inline-block"},
+            style={"width": "15%", "display": "inline-block"},
         ),
         dcc.Graph(id="indicator-graphic"),
     ],
@@ -103,48 +110,31 @@ app.layout = html.Div(
 
 @app.callback(
     Output("indicator-graphic", "figure"),
+    Input(component_id="data_name_input", component_property="value"),
     Input(component_id="show_figures", component_property="value"),
     Input(component_id="show_columns", component_property="value"),
-    Input(component_id="data_name_input", component_property="value"),
 )
-def update_fig(show_figures, show_columns, data_name_input):
+def update_fig(data_name_input, show_figures, show_columns):
+    global DATAFRAME
+    global available_ids
+    global NBR_KEY
     if data_name_input:
         path = os.path.join(DATA_DIR, data_name_input)
         print(f"path:{path}")
-        global df_bar
-        global available_ids
-        global NBR_KEY
-        df_bar = pd.read_csv(path)
-        df_bar["stock_id"] = df_bar["stock_id"].astype(str)
-        df_bar["date"] = pd.to_datetime(df_bar["date"])
-        df_bar["close"] = df_bar["close"].astype(int)
-        print(df_bar.info())
-        available_ids = df_bar["stock_id"].astype(str).unique()
+        DATAFRAME = pd.read_csv(path)
+
+        DATAFRAME["stock_id"] = DATAFRAME["stock_id"].astype(str)
+        DATAFRAME["date"] = pd.to_datetime(DATAFRAME["date"])
+        DATAFRAME["close"] = DATAFRAME["close"].astype(float)
+        print(DATAFRAME.info())
+        available_ids = DATAFRAME["stock_id"].astype(str).unique()
         NBR_KEY = len(available_ids)
 
     print(
         f"update fig, show_figures:{show_figures} show_columns:{show_columns}"
     )
-    fig = get_subplots_fig(int(show_columns), int(show_figures))
+    fig = get_subplots_fig(int(show_figures), int(show_columns))
     return fig
-
-
-# @app.callback(
-#     Output('indicator-graphic', 'figure'),
-#     Input(component_id='data_name_input', component_property='value'),
-# )
-# def update_datasets(data_name_input):
-#     path = os.path.join(DATA_DIR, data_name_input)
-#     print(f"path:{path}")
-#     global df_bar
-#     global available_ids
-#     global NBR_KEY
-#     df_bar = pd.read_csv(path)
-#     df_bar["date"] = pd.to_datetime(df_bar["date"])
-#     df_bar["close"] = df_bar["close"].astype(int)
-#     print(df_bar.info())
-#     available_ids = df_bar["stock_id"].astype(str).unique()
-#     NBR_KEY = len(available_ids)
 
 
 if __name__ == "__main__":
